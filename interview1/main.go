b89e9fb38ad8188e5404d2fd10d6270aa61affea
David O'Dell <dave@helix.re>
2025-02-05 15:25:00 -0800

    comments

--- interview1/main.go
+++ interview1/main.go
@@ -1,5 +1,9 @@
 package main
 
+// create a function that takes a list of integers and returns the integer closest to 0
+// if the closest int is negative and positive, return the positive one
+// if the list is empty, return 0
+
 func main() {
 	list := []int{10, -2, 2, 3, 4, 5}
 
